7e685e744287236b181db57625f46f7856c440d1
Cristiano Caon <cristiano.caon@hotmail.com>
2020-10-04 19:44:29 -0500

    Ceiling Fan Button Functionality

--- script.js
+++ script.js
@@ -11,3 +11,26 @@ covidBtn.addEventListener('click', function () {
         covid.setAttribute('visible', false);
     }
 });
+
+let fanBtn = document.querySelector('#fanBtn');
+let flag = true;
+
+fanBtn.addEventListener('click', function () {
+    console.log("Fan button clicked!!!");
+    let fan1 = document.querySelector('#fan1');
+    let fan2 = document.querySelector('#fan2');
+
+    if (fan1.getAttribute("loop") == "true" && fan2.getAttribute("loop") == "true") {
+        console.log("Works")
+        fan1.setAttribute('loop', "false");
+        fan2.setAttribute('loop', "false");
+        fan1.setAttribute('to', "0 0 0");
+        fan2.setAttribute('to', "0 90 0");
+    } else {
+        console.log(fan1.getAttribute("loop"), fan2.getAttribute("loop"))
+        fan1.setAttribute('loop', "true");
+        fan2.setAttribute('loop', "true");
+        fan1.setAttribute('to', "0 360 0");
+        fan2.setAttribute('to', "0 450 0");
+    }
+});
